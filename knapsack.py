9ddeb773995348a4e74d02f40da120f2b1afcf33
Andrew Baluyut <andrew.baluyut@gmail.com>
2021-04-21 20:15:58 -0800

    add file

---
+++ knapsack.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+import math
+import random
+
+
+class Item:
+    
+    def __init__(self, name: "item", weight: 0, value: 0):
+        self.name = name
+        self.weight = weight
+        self.value = value
+
+
+def main():
+    print('hello')
+
+
+if __name__ == "__main__":
+    main()
